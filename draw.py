5430644921b1ac9660ef338210dfa251c323ebcb
Sasha <akoroza@stuy.edu>
2019-02-06 15:34:45 -0500

    new

--- draw.py
+++ draw.py
@@ -1,4 +1,35 @@
 from display import *
 
+
+
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+    # octant 1 setup
+    x = x0
+    y = y0
+    A= y1-y0
+    B= -(x1-x0)
+    d= 2*A+B
+    while x<= x1:
+        display.plot(screen, color, x, y)
+        if d > 0:
+            y += 1
+            d+=2*B
+        x += 1
+        d+= 2*A
+
+    '''
+    # octant 2 setup
+    x = x0
+    y = y0
+    A= y1-y0
+    B= -(x1-x0)
+    d= A+2*B
+    while y<= y1:
+        display.plot(screen, color, x, y)
+        if d > 0:
+            x++
+            d+=2*A
+        y++
+        d+= 2*B
+
+    '''
--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-display(screen)
+draw_line(0,0,10,10,screen,color)
+#display(screen)
 save_extension(screen, 'img.png')
