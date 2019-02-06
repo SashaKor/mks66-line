974da6882b34b0da99d8550922e765822b3c1f6b
Sasha <akoroza@stuy.edu>
2019-02-06 15:36:08 -0500

    inchworm

--- draw.py
+++ draw.py
@@ -10,7 +10,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     B= -(x1-x0)
     d= 2*A+B
     while x<= x1:
-        display.plot(screen, color, x, y)
+        plot(screen, color, x, y)
         if d > 0:
             y += 1
             d+=2*B
@@ -25,7 +25,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     B= -(x1-x0)
     d= A+2*B
     while y<= y1:
-        display.plot(screen, color, x, y)
+        plot(screen, color, x, y)
         if d > 0:
             x++
             d+=2*A
