93f82bdba016997d0b5c96cf4fa35db00516bdc9
Sasha <akoroza@stuy.edu>
2019-02-07 12:02:54 -0500

    done

--- draw.py
+++ draw.py
@@ -3,19 +3,28 @@ from display import *
 
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    # octant 1 setup
+    #setting origin pt
     x = x0
     y = y0
     A= y1-y0
     B= -(x1-x0)
-    d= 2*A+B
-    while x<= x1:
-        plot(screen, color, x, y)
-        if d > 0:
-            y += 1
-            d+=2*B
-        x += 1
-        d+= 2*A
+
+    if(0 == B):
+        m= 10^8
+    else:
+        m= A/B
+    print(m)
+    #octant 1
+    if (m <= 1 and m >= 0 ):
+        print("here")
+        d= 2*A+B
+        while x<= x1:
+            plot(screen, color, x, y)
+            if d > 0:# error counting
+                y += 1
+                d+=2*B
+            x += 1
+            d += 2*A
 
     '''
     # octant 2 setup
@@ -23,13 +32,24 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     y = y0
     A= y1-y0
     B= -(x1-x0)
-    d= A+2*B
-    while y<= y1:
-        plot(screen, color, x, y)
-        if d > 0:
-            x++
-            d+=2*A
-        y++
-        d+= 2*B
-
+    preserved from before
     '''
+    if (m <= 1 and m >= 0 ):
+        d= A+2*B
+        while y<= y1:
+            plot(screen, color, x, y)
+            if d > 0:
+                x+=1
+                d+=2*A
+            y+=1
+            d+= 2*B
+
+    if (m <= 1 and m >= 0 ):
+        d= A+2*B
+        while y<= y1:
+            plot(screen, color, x, y)
+            if d > 0:
+                x+=1
+                d+=2*A
+            y+=1
+            d+= 2*B
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-draw_line(0,0,10,10,screen,color)
-#display(screen)
+draw_line(0,0,100,100,screen,color)
+draw_line(100,100,200,200,screen,color)
+display(screen)
 save_extension(screen, 'img.png')
