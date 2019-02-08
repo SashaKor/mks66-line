7b0278eaa9e3451ee8d4c9fddf8b14942516dbf8
Sasha <akoroza@stuy.edu>
2019-02-08 13:42:38 -0500

    vert line

--- draw.py
+++ draw.py
@@ -6,20 +6,22 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     #setting origin pt
     x = x0
     y = y0
-    A= y1-y0
-    B= -(x1-x0)
-    C= x1-x0
+    A = y1-y0
+    B = -(x1-x0)
+    C = x1-x0
 
     if (C == 0):
         m= 10^5 #handling case when div by zero
     else:
         m = A / C
 
+    #draw vertical line
     if m == 10^5:
-        pass
-        #draw vertical line
+        while y <= y1:
+            plot(screen, color, x, y)
+            y+=1
 
-    #octant 1
+    # octant 1
     if (m <= 1 and m >= 0 ):
         print("octant 1 m: "+str(m))
         d = 2*A+B
@@ -53,16 +55,16 @@ def draw_line( x0, y0, x1, y1, screen, color ):
                 y -= 1
                 d += 2*B
             x += 1
-            d += 2*A
+            d -= 2*A
 
     #Octant 8 like octant 2 but negative
     if (m < -1 and m != 10^5):
         print("octant 8 m: "+ str(m))
         d= A+2*B
-        while y>= y1:
+        while y >= y1:
             plot(screen, color, x, y)
             if d > 0:
                 x+=1
-                d+=2*A
+                d-=2*A
             y -=1
             d += 2*B
--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-'''
+
 draw_line(0,0,100,100,screen,color)
 draw_line(100,100,200,200,screen,color)
 draw_line(0,0,200,0,screen,color)
-draw_line(0,0,0,200,screen,color)
+#draw_line(0,0,0,200,screen,color)
 draw_line(0,0,5,100,screen,color)
-'''
+
 draw_line(50,40,80,20,screen,color)
+draw_line(0,0,0,100,screen,color)
+
 display(screen)
 save_extension(screen, 'img.png')
